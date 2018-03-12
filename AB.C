d8fcc123c39f299fd697ff81677fc9b0a3e584e6
kalaipraba <35525844+kalaipraba@users.noreply.github.com>
2018-03-12 15:56:31 +0530

    AB.C

---
+++ AB.C
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main (void)
+{
+      int a = 100;
+      int b = 2;
+      int c = 25;
+      int result;
+
+      result = a *B/C;      
+      printf ("a *b/c = %i\n", result);
+
+      result = b / c;       
+      printf ("b/c = %i\n", result);
+
+      result = a *b;      
+      printf ("a*b = %i\n", result);
+      return 0;
+}
